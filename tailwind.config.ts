be690fa10d5627cc8d08fafd3dfc767038288e04
Bryant Le <bryantle2021@gmail.com>
2024-10-31 01:43:04 -0500

    feat: add fonts and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,25 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
 	content: ["./app/**/*.{ts,tsx}"],
-	theme: {},
+	theme: {
+		colors: {
+			"jet-black": "#0A0A0A",
+			"royal-yellow": "#FADA5E",
+			bruh: {
+				eye: "#222222",
+				mouth: "#FC4141",
+			},
+		},
+		fontFamily: {
+			argon: ["MonaspaceArgonVariable", ...defaultTheme.fontFamily.mono],
+			krypton: ["MonaspaceKryptonVariable", ...defaultTheme.fontFamily.mono],
+			neon: ["MonaspaceNeonVariable", ...defaultTheme.fontFamily.mono],
+			radon: ["MonaspaceRadonVariable", ...defaultTheme.fontFamily.mono],
+			xenon: ["MonaspaceXenonVariable", ...defaultTheme.fontFamily.mono],
+		},
+		extend: {},
+	},
 	plugins: [],
 } satisfies Config;
